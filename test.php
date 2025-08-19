1ce2c32bc17ca4eabb7809e387f1a8b15a1cc0d3
Kuruzukel <michael333.jc@gmail.com>
2025-08-19 20:16:37 +0800

    mishan

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+require 'vendor/autoload.php';
+
+$client = new MongoDB\Client("mongodb://localhost:27017");
+$dbs = $client->listDatabases();
+
+foreach ($dbs as $db) {
+    echo $db->getName(), "<br>";
+}
+?>
