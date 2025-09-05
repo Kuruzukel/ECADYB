a64ddc379b38cb1ef0ba39df25e6ebb4b93b2cb0
Kuruzukel <michael333.jc@gmail.com>
2025-09-05 20:52:18 +0800

    mishan

--- Connection/UploadCover.php
+++ Connection/UploadCover.php
@@ -3,9 +3,7 @@
 
 ob_start();
 
-// -------------------------------
 // Headers (for Railway / CORS)
-// -------------------------------
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
