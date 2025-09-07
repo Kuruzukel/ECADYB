d184d6841b04f2a338897f6aba72876475dad6b5
Kuruzukel <michael333.jc@gmail.com>
2025-09-07 19:58:11 +0800

    misha

--- Connection/MongoConnect.php
+++ Connection/MongoConnect.php
@@ -7,7 +7,6 @@ use MongoDB\Client;
 $mongoUrl = getenv('MONGO_URL') ?: 'mongodb://mongo:tIEbUVpHiKhDZTkghDEMqERbLDdsDRnX@shortline.proxy.rlwy.net:56957';
 $client   = new Client($mongoUrl);
 
-// ----------------------
 // Departments Database
 // ----------------------
 $departmentsDB     = $client->Departments;
