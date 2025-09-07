1c4c87408f62ca1839868e10e6896a974db2b80f
Kuruzukel <michael333.jc@gmail.com>
2025-09-07 19:56:21 +0800

    mishan

--- Connection/DeleteStudent.php
+++ Connection/DeleteStudent.php
@@ -21,7 +21,6 @@ try {
 }
 
 // Read JSON POST data
-// ----------------------
 $data = json_decode(file_get_contents('php://input'), true);
 
 $studentId = isset($data['student_id']) ? trim($data['student_id']) : null;
