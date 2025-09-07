b2b1a665d298f84b2648135696d42edfc0332c51
Kuruzukel <michael333.jc@gmail.com>
2025-09-07 19:54:53 +0800

    misha

--- Connection/DeleteCover.php
+++ Connection/DeleteCover.php
@@ -35,7 +35,6 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     respond(false, 'Invalid request method');
 }
 
-// -------------------------------
 // Load dependencies
 require __DIR__ . '/../vendor/autoload.php';
 if (file_exists(__DIR__ . '/BunnyConfig.php')) {
