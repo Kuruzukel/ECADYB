6310e78b40b2afb4b8ab5ba0e548aeef4355b50e
Kuruzukel <michael333.jc@gmail.com>
2025-09-07 19:52:53 +0800

    misha

--- Connection/DeleteCover.php
+++ Connection/DeleteCover.php
@@ -136,7 +136,6 @@ try {
         ];
     }
 
-    // ===============================
     // Update MongoDB (unset fields but keep slot record)
     $collection->updateOne(
         ['template' => $template, 'slot' => $slot],
