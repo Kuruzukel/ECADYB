0f3282614bb82bc682fbcbd680a1cf60cea50969
Kuruzukel <michael333.jc@gmail.com>
2025-09-13 18:31:04 +0800

    mishan

--- Public/assets/js/Login.js
+++ Public/assets/js/Login.js
@@ -150,7 +150,6 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Close modal when clicking outside
   const errorModal = document.getElementById("errorModal");
   if (errorModal) {
     errorModal.addEventListener("click", function (e) {
