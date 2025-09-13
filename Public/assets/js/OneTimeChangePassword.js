e6546ea4ea343215885ef4a3a175d02299fb429f
Kuruzukel <michael333.jc@gmail.com>
2025-09-13 18:33:18 +0800

    mishan

--- Public/assets/js/OneTimeChangePassword.js
+++ Public/assets/js/OneTimeChangePassword.js
@@ -134,7 +134,6 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Add transition to back button
   const backButton = document.querySelector('button[type="back"]');
   if (backButton) {
     backButton.addEventListener("click", function (e) {
