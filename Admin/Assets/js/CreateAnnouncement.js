11a76e0859871e2053bba71f57b80e5e44c97287
Kuruzukel <michael333.jc@gmail.com>
2025-09-20 15:31:20 +0800

    mishan

--- Admin/Assets/js/CreateAnnouncement.js
+++ Admin/Assets/js/CreateAnnouncement.js
@@ -261,7 +261,6 @@ function initializeModal() {
 
     document.addEventListener("keydown", function (e) {
       if (e.key === "Escape" && modalOverlay.style.display === "flex") {
-        // Just hide the modal - don't restore content to prevent confirm modal from showing
         hideModal();
       }
     });
