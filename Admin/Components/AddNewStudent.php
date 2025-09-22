2d4beb4507c036a7450a9edc33b8f3ae8d8e4ff7
Kuruzukel <michael333.jc@gmail.com>
2025-09-22 15:18:52 +0800

    misha

--- Admin/Components/AddNewStudent.php
+++ Admin/Components/AddNewStudent.php
@@ -54,7 +54,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         exit;
     }
 
-    // ----------------------
     // Prepare student document
     // ----------------------
     $student = [
