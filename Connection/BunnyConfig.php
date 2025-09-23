8c33b01290dfe97478a07832c0ecf1fdb8d04911
Kuruzukel <michael333.jc@gmail.com>
2025-09-23 13:58:22 +0800

    misha

--- Connection/BunnyConfig.php
+++ Connection/BunnyConfig.php
@@ -1,6 +1,5 @@
 <?php
 
-// As constants (preferred)
 if (!defined('BUNNY_STORAGE_ZONE')) define('BUNNY_STORAGE_ZONE', 'ecadyb');
 if (!defined('BUNNY_ACCESS_KEY')) define('BUNNY_ACCESS_KEY', 'db959684-d63e-41f4-a1c7de4737a9-2dd8-41fb'); // write access key
 if (!defined('BUNNY_CDN_HOST')) define('BUNNY_CDN_HOST', 'https://ECADYB.b-cdn.net');
