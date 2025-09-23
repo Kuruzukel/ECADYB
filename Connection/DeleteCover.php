b2ac5efcb00032d0d15974c5e479b9b27f8cc662
Kuruzukel <michael333.jc@gmail.com>
2025-09-23 14:00:36 +0800

    misha

--- Connection/DeleteCover.php
+++ Connection/DeleteCover.php
@@ -7,7 +7,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
-// Preflight OPTIONS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
