c206a0784159386e21a244375f4d90e39474f59c
Kuruzukel <michael333.jc@gmail.com>
2025-09-23 14:08:29 +0800

    misha

--- Connection/FetchCovers.php
+++ Connection/FetchCovers.php
@@ -1,15 +1,12 @@
 <?php
-// Fetch Yearbook Covers API
 
 ob_start();
 
-// Headers (for Railway / CORS)
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
-// Preflight OPTIONS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
