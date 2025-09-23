6ff8804f2a2240536995f7c52003638f77c84413
Kuruzukel <michael333.jc@gmail.com>
2025-09-23 13:58:40 +0800

    misha

--- Connection/BunnyConfig.php
+++ Connection/BunnyConfig.php
@@ -4,7 +4,6 @@ if (!defined('BUNNY_STORAGE_ZONE')) define('BUNNY_STORAGE_ZONE', 'ecadyb');
 if (!defined('BUNNY_ACCESS_KEY')) define('BUNNY_ACCESS_KEY', 'db959684-d63e-41f4-a1c7de4737a9-2dd8-41fb');
 if (!defined('BUNNY_CDN_HOST')) define('BUNNY_CDN_HOST', 'https://ECADYB.b-cdn.net');
 
-// Also expose in $GLOBALS for older includes
 $GLOBALS['BUNNY_STORAGE_ZONE'] = BUNNY_STORAGE_ZONE;
 $GLOBALS['BUNNY_ACCESS_KEY'] = BUNNY_ACCESS_KEY;
 $GLOBALS['BUNNY_CDN_HOST'] = BUNNY_CDN_HOST;
