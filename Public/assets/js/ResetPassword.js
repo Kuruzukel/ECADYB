49cb74a678ce6c2a3172d91648472f8d4a9b533d
Kuruzukel <michael333.jc@gmail.com>
2025-09-24 18:39:30 +0800

    mishan

--- Public/assets/js/ResetPassword.js
+++ Public/assets/js/ResetPassword.js
@@ -145,7 +145,7 @@ window.addEventListener("DOMContentLoaded", () => {
       document.body.classList.add("page-transition-out");
 
       setTimeout(() => {
-        window.location.href = "/ECADYB/Public/Components/Login.php";
+        window.location.href = "/Public/Components/Login.php";
       }, 1000);
     });
   }
