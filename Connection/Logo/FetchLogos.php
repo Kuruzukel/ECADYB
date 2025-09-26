779ea12bd12744c4b2ea696209ead43c47265cd8
Kuruzukel <michael333.jc@gmail.com>
2025-09-26 13:40:55 +0800

    mishan

--- Connection/Logo/FetchLogos.php
+++ Connection/Logo/FetchLogos.php
@@ -6,7 +6,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
-// Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
