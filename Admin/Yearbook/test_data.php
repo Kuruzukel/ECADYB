d8fc5d104170fb36c6e0900ee1efb78a210e47d0
Kuruzukel <michael333.jc@gmail.com>
2025-10-02 12:51:25 +0800

    misha

--- Admin/Yearbook/test_data.php
+++ Admin/Yearbook/test_data.php
@@ -43,7 +43,6 @@ try {
         throw new Exception('Cover not found for template ' . $template . ' and slot ' . $slot);
     }
 
-    // Format the response
     $response = [
         '_id' => (string)$cover['_id'],
         'slot' => (int)$cover['slot'],
