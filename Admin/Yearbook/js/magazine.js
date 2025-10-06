4e638b5338c06fb546b3f22ef1e6d3b95bae5c07
Kuruzukel <michael333.jc@gmail.com>
2025-10-06 14:08:56 +0800

    mishan

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -410,7 +410,6 @@ function loadPage(page, pageElement) {
 
                   modal.addClass("active");
 
-                  // Handle thumbnail clicks
                   $thumbnails.off("click").on("click", function (e) {
                     e.stopPropagation(); // Prevent modal from closing
                     e.preventDefault();
