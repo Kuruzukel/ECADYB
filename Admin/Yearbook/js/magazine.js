a2283d910cfd017f9b736e5736ceba95f41bfe6b
Kuruzukel <michael333.jc@gmail.com>
2025-10-06 14:08:47 +0800

    misha

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -408,7 +408,6 @@ function loadPage(page, pageElement) {
                     }
                   });
 
-                  // Show modal
                   modal.addClass("active");
 
                   // Handle thumbnail clicks
