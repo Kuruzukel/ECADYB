5884e552da9c1cb1bdd8bad77b775e7095dae0c1
Kuruzukel <michael333.jc@gmail.com>
2025-10-07 16:42:29 +0800

    misha

--- Connection/Student/UpdateStatus.php
+++ Connection/Student/UpdateStatus.php
@@ -47,7 +47,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    // Use the correct BatchTemplate database
     $dbName = "BatchTemplate" . $template;
     $db = $client->$dbName;
 
