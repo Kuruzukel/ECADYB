2c5eba9db8b75b5a46e2aa74d7f9621dc15339a1
Kuruzukel <michael333.jc@gmail.com>
2025-10-07 16:36:46 +0800

    misha

--- Connection/Student/BulkUpdateStatus.php
+++ Connection/Student/BulkUpdateStatus.php
@@ -53,7 +53,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         $coll = $db->$collection;
 
-        // Build filter for bulk update
         $filter = [];
 
         // Apply status filter if provided
