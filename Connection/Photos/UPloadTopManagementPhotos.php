b07fe767b3ec75d2151fe01b02aea2b1fc7ce917
Kuruzukel <michael333.jc@gmail.com>
2025-10-07 16:47:23 +0800

    misha

--- Connection/Photos/UPloadTopManagementPhotos.php
+++ Connection/Photos/UPloadTopManagementPhotos.php
@@ -174,7 +174,6 @@ try {
 
         if (connection_aborted()) {
             $uploadCancelled = true;
-            // If upload was successful but client disconnected, delete the file from BunnyCDN
             if ($response !== false && $httpCode >= 200 && $httpCode < 300) {
                 error_log("UploadTopManagementPhotos.php deleting file from BunnyCDN due to cancellation: $storageUrl");
                 $deleteCh = curl_init($storageUrl);
