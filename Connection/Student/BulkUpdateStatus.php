8e923bad304a5a02843cc6ff76eba3b05b24f3a9
Kuruzukel <michael333.jc@gmail.com>
2025-10-07 16:37:10 +0800

    misha

--- Connection/Student/BulkUpdateStatus.php
+++ Connection/Student/BulkUpdateStatus.php
@@ -65,7 +65,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $update = ['$set' => ['status' => $status]];
 
-        // Perform bulk update
         $result = $coll->updateMany($filter, $update);
 
         error_log("BulkUpdateStatus - Database: " . $dbName . ", Collection: " . $collection . ", Status: " . $status);
