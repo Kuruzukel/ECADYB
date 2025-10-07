b912365b4bcd15812e8a5e9a5bc1d95927bdd02a
Kuruzukel <michael333.jc@gmail.com>
2025-10-07 16:47:40 +0800

    misha

--- Connection/Photos/UPloadTopManagementPhotos.php
+++ Connection/Photos/UPloadTopManagementPhotos.php
@@ -204,7 +204,6 @@ try {
 
         if (connection_aborted()) {
             $uploadCancelled = true;
-            // Delete file from BunnyCDN since we're cancelling before MongoDB
             error_log("UploadTopManagementPhotos.php deleting file from BunnyCDN due to cancellation before MongoDB: $storageUrl");
             $deleteCh = curl_init($storageUrl);
             curl_setopt_array($deleteCh, [
