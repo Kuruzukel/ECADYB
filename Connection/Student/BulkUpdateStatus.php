fd5766c0eef4bfc14c67cc921d35295d89b851ea
Kuruzukel <michael333.jc@gmail.com>
2025-10-07 16:36:37 +0800

    mishan

--- Connection/Student/BulkUpdateStatus.php
+++ Connection/Student/BulkUpdateStatus.php
@@ -47,7 +47,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    // Use the correct BatchTemplate database
     $dbName = "BatchTemplate" . $template;
     $db = $client->$dbName;
 
@@ -56,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Build filter for bulk update
         $filter = [];
-        
+
         // Apply status filter if provided
         if ($statusFilter && $statusFilter !== '') {
             if ($statusFilter === 'active') {
