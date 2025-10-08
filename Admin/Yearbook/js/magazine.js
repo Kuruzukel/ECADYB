1ccb1046a1021a55c335097ce29ffc9518691bd7
Kuruzukel <michael333.jc@gmail.com>
2025-10-08 10:15:08 +0800

    mishan

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -601,7 +601,6 @@ function loadPage(page, pageElement) {
               text: "Honors and Achievements",
             });
 
-            // Add click handler for the card
             card.on("click", function () {
               var modal = $(".student-modal");
               var closeBtn = $(".close-modal");
