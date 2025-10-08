92b0e86c6b831225aa33c5198cf48ece7b76bbdc
Kuruzukel <michael333.jc@gmail.com>
2025-10-08 10:14:50 +0800

    misha

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -593,7 +593,6 @@ function loadPage(page, pageElement) {
             });
             studentImg.append(placeholderImg);
 
-            // Add student name (default placeholder)
             var studentName = $("<h3/>", {
               text: "Student Name",
             });
