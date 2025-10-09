623fca193e19c208aed0ec1e27c94f3c192b1b43
Kuruzukel <michael333.jc@gmail.com>
2025-10-09 19:51:56 +0800

    misha

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -1202,7 +1202,6 @@ function generatePageThumbnail(page, pageElement) {
     }
 
     if (page >= 7 && studentContent.length) {
-      // Create semi-transparent overlay
       ctx.fillStyle = "rgba(255, 255, 255, 0.95)";
       ctx.fillRect(2, 2, canvas.width - 4, canvas.height - 4);
 
