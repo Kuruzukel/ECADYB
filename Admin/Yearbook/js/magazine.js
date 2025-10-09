65851560708472105c14cf305481d0af2031db9f
Kuruzukel <michael333.jc@gmail.com>
2025-10-09 19:46:09 +0800

    mishan

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -799,7 +799,6 @@ function loadLargePage(page, pageElement) {
     prevImg.remove();
   });
 
-  // Check if we're loading the first or last page and use cover data if available
   var totalPages = $(".magazine").turn("pages");
 
   // Wait for coverData to be available if it's not yet loaded, but don't wait indefinitely
