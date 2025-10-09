153aead252b3ed3b160ea10400485fc07a7361a7
Kuruzukel <michael333.jc@gmail.com>
2025-10-09 19:45:01 +0800

    mishan

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -691,7 +691,6 @@ function loadPage(page, pageElement) {
     );
     img.attr("src", coverData.background_url);
   } else {
-    // Show a placeholder when no image is available
     console.log("No image available for page:", page, "Showing placeholder");
     // Create a placeholder with a colored background
     img.attr(
