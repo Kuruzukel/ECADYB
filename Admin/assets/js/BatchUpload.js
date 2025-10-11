25c83b497c3811ed2505eea278731728c1eff74c
Kuruzukel <michael333.jc@gmail.com>
2025-10-11 18:29:53 +0800

    mishan

--- Admin/assets/js/BatchUpload.js
+++ Admin/assets/js/BatchUpload.js
@@ -291,7 +291,6 @@ window.addEventListener("DOMContentLoaded", () => {
               if (notificationScript) {
                 eval(notificationScript.textContent);
 
-                // Clear the select all checkbox state when CSV is successfully uploaded
                 // This ensures the select all checkbox is unchecked on the StudentList page
                 // since all newly uploaded students have "Pending" status
                 localStorage.removeItem("selectAllState");
