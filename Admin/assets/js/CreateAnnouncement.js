25cda2bddf7a8aa323a059453b8c60b90de28297
Kuruzukel <michael333.jc@gmail.com>
2025-10-11 18:31:40 +0800

    mishan

--- Admin/assets/js/CreateAnnouncement.js
+++ Admin/assets/js/CreateAnnouncement.js
@@ -464,7 +464,7 @@ function showNotification(message, type = "success") {
     setTimeout(() => {
       notif.remove();
       notificationTimeout = null;
-      currentOperation = null; // Clear current operation
+      currentOperation = null;
     }, 500);
   }, duration);
 }
