20521850463b24e7697bed946a9f60c8aaed1bcf
Kuruzukel <michael333.jc@gmail.com>
2025-10-11 19:38:31 +0800

    misha

--- Admin/Departments/BusinessAdministration.php
+++ Admin/Departments/BusinessAdministration.php
@@ -2,27 +2,28 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>College of Business Administration</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <link href="../Flipbook/turn.js/dist/style.css" rel="stylesheet">
-  <link href="../Departments/assets/css/BusinessAdministration.css" rel="stylesheet">
-  </head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>College of Business Administration</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="../Flipbook/turn.js/dist/style.css" rel="stylesheet">
+    <link href="../Departments/assets/css/BusinessAdministration.css" rel="stylesheet">
+</head>
 
 <body>
-  <div class="container">
-    <div class="catalog-root">
-      <div class="catalog-app">
-        <iframe src="../Yearbook/index.html?department=BSBA" width="100%" height="100%" style="border: none; min-height: 680px;"></iframe>
-      </div>
-    </div>
+    <div class="container">
+        <div class="catalog-root">
+            <div class="catalog-app">
+                <iframe src="../Yearbook/index.html?department=BSBA" width="100%" height="100%"
+                    style="border: none; min-height: 670px;"></iframe>
+            </div>
+        </div>
 
-    <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
-    <script src="../Departments/assets/js/BusinessAdministration.js"></script>
-  </div>
+        <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
+        <script src="../Departments/assets/js/BusinessAdministration.js"></script>
+    </div>
 </body>
 
 </html>
--- Admin/Departments/InformationSystem.php
+++ Admin/Departments/InformationSystem.php
@@ -8,21 +8,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="../Flipbook/turn.js/dist/style.css" rel="stylesheet">
     <link href="../Departments/assets/css/InformationSystem.css" rel="stylesheet">
-    </head>
+</head>
 
 <body>
-  <div class="container">
-    <div class="catalog-root">
-      <div class="catalog-app">
-        <iframe src="../Yearbook/index.html?department=BSIS" width="100%" height="100%" style="border: none; min-height: 680px;"></iframe>
-      </div>
-    </div>
+    <div class="container">
+        <div class="catalog-root">
+            <div class="catalog-app">
+                <iframe src="../Yearbook/index.html?department=BSIS" width="100%" height="100%"
+                    style="border: none; min-height: 670px;"></iframe>
+            </div>
+        </div>
 
-    <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
-    <script src="../Departments/assets/js/InformationSystem.js"></script>
-  </div>
+        <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
+        <script src="../Departments/assets/js/InformationSystem.js"></script>
+    </div>
 </body>
 
 </html>
