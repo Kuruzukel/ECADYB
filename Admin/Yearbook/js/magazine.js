5d2e87aa83f8f4c94d00bb96251a8fc282e4493b
Kuruzukel <michael333.jc@gmail.com>
2025-10-13 12:24:46 +0800

    misha

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -735,7 +735,6 @@ function loadPage(page, pageElement) {
 
         var template = coverData && coverData.template ? coverData.template : 1;
 
-        // Calculate which students we need for this yearbook page
         var studentsPerYearbookPage = 6;
         var studentStartIndex = (page - 7) * studentsPerYearbookPage; // First student index for this page
         var studentEndIndex = studentStartIndex + studentsPerYearbookPage; // Last student index + 1
