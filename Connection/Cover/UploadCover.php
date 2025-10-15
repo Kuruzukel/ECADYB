ff0a580fe96baa5577f24bf076d4486d01aab26f
Kuruzukel <michael333.jc@gmail.com>
2025-10-15 13:40:18 +0800

    misha

--- Connection/Cover/UploadCover.php
+++ Connection/Cover/UploadCover.php
@@ -365,8 +365,6 @@ try {
         curl_exec($deleteCh);
         curl_close($deleteCh);
 
-        // Thumbnail deletion removed
-
         $collection->deleteOne(['_id' => $document['_id']]);
         respond(false, 'Upload cancelled');
     }
