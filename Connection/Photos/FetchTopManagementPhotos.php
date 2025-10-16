228e82006038c0a91922272db0ecc2554a42ea94
Kuruzukel <michael333.jc@gmail.com>
2025-10-16 18:46:09 +0800

    mishan

--- Connection/Photos/FetchTopManagementPhotos.php
+++ Connection/Photos/FetchTopManagementPhotos.php
@@ -3,7 +3,6 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-// Prevent caching to ensure fresh data after CSV upload
 header('Cache-Control: no-cache, no-store, must-revalidate');
 header('Pragma: no-cache');
 header('Expires: 0');
