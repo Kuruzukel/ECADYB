9b809b90266ebd490f04f0dcaaab5ffce9ee0cdf
Kuruzukel <michael333.jc@gmail.com>
2025-10-17 23:59:44 +0800

    mishan

--- Admin/Departments/Maritime.php
+++ Admin/Departments/Maritime.php
@@ -6,15 +6,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>College of Maritime Education</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <link href="../Flipbook/turn.js/dist/style.css" rel="stylesheet">
-  <link href="../Departments/assets/css/Maritime.css" rel="stylesheet">
+  <link href="/ECADYB/Admin/Flipbook/turn.js/dist/style.css" rel="stylesheet">
+  <link href="/ECADYB/Admin/Departments/assets/css/Maritime.css" rel="stylesheet">
 </head>
 
 <body>
+  <?php
+  // Prevent direct access to this file
+  if (!defined('ADMIN_DASHBOARD_INCLUDED')) {
+    // If accessed directly, redirect to the proper route
+    header('Location: /ECADYB/Admin');
+    exit;
+  }
+  ?>
   <div class="container">
     <div class="catalog-root">
       <div class="catalog-app">
-        <iframe src="../Yearbook/index.html?department=BSME" width="100%" height="100%"
+        <iframe src="/ECADYB/Admin/Yearbook/index.html?department=BSME" width="100%" height="100%"
           style="border: none; min-height: 670px;"></iframe>
       </div>
     </div>
@@ -22,7 +30,7 @@
     <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
-    <script src="../Departments/assets/js/Maritime.js"></script>
+    <script src="/ECADYB/Admin/Departments/assets/js/Maritime.js"></script>
   </div>
 </body>
 
