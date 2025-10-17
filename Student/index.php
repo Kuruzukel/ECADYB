38a6b343fc0be7ef16e0e9cad17718deb4c3861f
Kuruzukel <michael333.jc@gmail.com>
2025-10-17 22:31:54 +0800

    misha

--- Student/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Set the correct base path for assets
-$_SERVER['SCRIPT_NAME'] = '/ECADYB/Student/Components/StudentDashboard.php';
-
-// Include the Student Dashboard
-require __DIR__ . '/Components/StudentDashboard.php';
-?>
-
