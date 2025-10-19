e112e9b77f2d4d43f63e52729fa09cefe3f5e3b4
Kuruzukel <michael333.jc@gmail.com>
2025-10-19 18:49:49 +0800

    misha

--- Admin/Departments/Tourism.php
+++ Admin/Departments/Tourism.php
@@ -2,35 +2,34 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>College of Tourism Management</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="<?= $basePath ?>/Admin/Flipbook/turn.js/dist/style.css" rel="stylesheet">
-    <link href="<?= $basePath ?>/Admin/Departments/assets/css/Tourism.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>College of Tourism Management</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link href="<?= $basePath ?>/Admin/Flipbook/turn.js/dist/style.css" rel="stylesheet">
+  <link href="<?= $basePath ?>/Admin/Departments/assets/css/Tourism.css" rel="stylesheet">
 </head>
 
 <body>
-    <?php
+  <?php
   if (!defined('ADMIN_DASHBOARD_INCLUDED')) {
-    // If accessed directly, redirect to the proper route
     header('Location: ../');
     exit;
   }
   ?>
-    <div class="container">
-        <div class="catalog-root">
-            <div class="catalog-app">
-                <iframe src="<?= $basePath ?>/Admin/Yearbook/index.html?department=BSTM" width="100%" height="100%"
-                    style="border: none; min-height: 670px;"></iframe>
-            </div>
-        </div>
-
-        <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
-        <script src="<?= $basePath ?>/Admin/Departments/assets/js/Tourism.js"></script>
+  <div class="container">
+    <div class="catalog-root">
+      <div class="catalog-app">
+        <iframe src="<?= $basePath ?>/Admin/Yearbook/index.html?department=BSTM" width="100%" height="100%"
+          style="border: none; min-height: 670px;"></iframe>
+      </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
+    <script src="<?= $basePath ?>/Admin/Departments/assets/js/Tourism.js"></script>
+  </div>
 </body>
 
 </html>
