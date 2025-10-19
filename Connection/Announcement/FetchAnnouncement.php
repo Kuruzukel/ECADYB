bd165606080d48220c8316ae21153aa12568d9b3
Kuruzukel <michael333.jc@gmail.com>
2025-10-19 18:50:31 +0800

    misha

--- Connection/Announcement/FetchAnnouncement.php
+++ Connection/Announcement/FetchAnnouncement.php
@@ -1,5 +1,4 @@
 <?php
-// Set headers first to allow CORS
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, OPTIONS');
