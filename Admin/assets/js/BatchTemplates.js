cfce87e74a72857773b4f64799b17fe526cd25da
Kuruzukel <michael333.jc@gmail.com>
2025-10-22 12:12:06 +0800

    misha

--- Admin/assets/js/BatchTemplates.js
+++ Admin/assets/js/BatchTemplates.js
@@ -1682,7 +1682,6 @@ function initializeSectionUploadBoxes(section, currentXhrs, isUploadCancelled) {
             if (xhrIndex > -1) {
               currentUploadControllers.splice(xhrIndex, 1);
             }
-            // Remove from pending
             const pendingIndex = pendingUploads.findIndex(
               (f) =>
                 f.slot === slot && f.side === side && f.batch_year === batchYear
