06e9fbc889448da35ad1bae4f38cf619039f5e56
Kuruzukel <michael333.jc@gmail.com>
2025-10-22 12:29:26 +0800

    mishan

--- Admin/Yearbook/test-php.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'success' => true,
-    'message' => 'PHP is working!',
-    'php_version' => phpversion(),
-    'server' => $_SERVER['SERVER_SOFTWARE'] ?? 'Unknown'
-]);
-
