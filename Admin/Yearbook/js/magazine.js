65ef6f8eb7baa1acf50d607efee722309cb4c485
Kuruzukel <michael333.jc@gmail.com>
2025-10-23 19:02:29 +0800

    msiha

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -435,7 +435,6 @@ function loadStudentsForPage(
           template,
           nextApiPage,
           function (nextResponse) {
-            // Enhanced error handling for next page
             if (
               nextResponse &&
               nextResponse.success &&
