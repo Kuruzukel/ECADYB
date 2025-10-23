dae5b71ad637258ba4dd8ccbe76a917eeaa12bd2
Kuruzukel <michael333.jc@gmail.com>
2025-10-23 19:04:14 +0800

    mishan

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -1231,7 +1231,6 @@ function loadPage(page, pageElement) {
 function zoomTo(event) {
   setTimeout(function () {
     // } else {
-    //   if ($(".magazine-viewport").zoom("value") == 1) {
     //     $(".magazine-viewport").zoom("zoomIn", event);
     //   } else {
     //     $(".magazine-viewport").zoom("zoomOut");
