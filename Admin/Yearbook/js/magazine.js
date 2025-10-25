aa95ff2714bfea1c0f35d02219e442f76c5bdbf9
Kuruzukel <michael333.jc@gmail.com>
2025-10-25 15:27:13 +0800

    misha

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -1972,7 +1972,6 @@ function addRegion(region, pageElement) {
 }
 
 function regionClick(event) {
-  //       $(".magazine-viewport").data().regionClicked = false;
   //     }, 100);
   //     var regionType = $.trim(region.attr("class").replace("region", ""));
   //     return processRegion(region, regionType);
