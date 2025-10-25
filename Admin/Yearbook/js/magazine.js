53e6a4a8b986e96d201b0cc1af985963b486041a
Kuruzukel <michael333.jc@gmail.com>
2025-10-25 15:35:11 +0800

    mishan

--- Admin/Yearbook/js/magazine.js
+++ Admin/Yearbook/js/magazine.js
@@ -329,7 +329,6 @@ function findAndNavigateToStudent(
               }
             }
 
-            // Helper function to notify parent window
             function notifyNavigationComplete() {
               console.log(
                 "📍 Sending navigation complete signal to parent window"
