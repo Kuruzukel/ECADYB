a2c2b34ab519a2682f27314ac11c7735fb3ac440
Kuruzukel <michael333.jc@gmail.com>
2025-10-28 18:02:39 +0800

    misha

--- Admin/assets/js/BatchTemplates.js
+++ Admin/assets/js/BatchTemplates.js
@@ -2461,7 +2461,6 @@ async function captureAllYearbookPages(
             );
             capturedPages.push(canvas);
             captureCount++;
-            // Report progress
             if (onPageProgress) {
               onPageProgress(captureCount, approxTotalCaptures);
             }
