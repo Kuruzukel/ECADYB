6f08e93071f015d1217a04e421639df4225c9f14
Kuruzukel <michael333.jc@gmail.com>
2025-10-31 14:53:08 +0800

    misha

--- Public/assets/js/ChangePassword.js
+++ Public/assets/js/ChangePassword.js
@@ -36,7 +36,6 @@ function setupEventListeners() {
     handleFormSubmission();
   });
 
-  // Back button
   const backButton = document.querySelector('button[type="back"]');
   if (backButton) {
     backButton.addEventListener("click", function (e) {
