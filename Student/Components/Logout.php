06815c8fa5edc6deea32ca959b20a53dc6839413
Kuruzukel <michael333.jc@gmail.com>
2025-11-01 14:28:27 +0800

    mishan

--- Student/Components/Logout.php
+++ Student/Components/Logout.php
@@ -4,7 +4,6 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 require_once __DIR__ . '/../../Connection/Configuration/config.php';
 
-// Remove active session from MongoDB before destroying
 if (isset($_SESSION['student_id'])) {
     try {
         require_once __DIR__ . '/../../vendor/autoload.php';
