9790d0d89fa34d4c30f9a70e4c596b3c5782d701
Kuruzukel <michael333.jc@gmail.com>
2025-11-02 15:42:22 +0800

    misha

--- Student/Yearbook/FetchCoverData.php
+++ Student/Yearbook/FetchCoverData.php
@@ -22,7 +22,6 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use MongoDB\Client;
 
-// Register shutdown function to catch fatal errors
 register_shutdown_function(function () {
     $error = error_get_last();
     if ($error !== null && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
