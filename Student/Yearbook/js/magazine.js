e2ca6934a5fd61351ebc86f0dbbc5c33be6cd5c7
Kuruzukel <michael333.jc@gmail.com>
2025-11-02 15:34:43 +0800

    mishan

--- Student/Yearbook/js/magazine.js
+++ Student/Yearbook/js/magazine.js
@@ -1439,7 +1439,6 @@ function loadPage(page, pageElement) {
 
 function zoomTo(event) {
   setTimeout(function () {
-    // if ($(".magazine-viewport").data().regionClicked) {
     //   $(".magazine-viewport").data().regionClicked = false;
     // } else {
     //   if ($(".magazine-viewport").zoom("value") == 1) {
