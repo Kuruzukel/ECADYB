de7a92d4f8837cc53b44b206ddec759dd6fb1bd0
Kuruzukel <michael333.jc@gmail.com>
2025-11-04 17:08:17 +0800

    mishan

--- Student/assets/js/StudentDashboard.js
+++ Student/assets/js/StudentDashboard.js
@@ -1285,7 +1285,6 @@ function initMemoriesCarousel() {
     if (e.target.classList.contains("carousel-img")) {
       stopAutoSlide();
 
-      // Show lightbox with the image
       showImageLightbox(e.target.src);
     }
   });
